1bb1c9526f03b060891aef61f60801aaadd66deb
joaovik81 <54294143+joaovik81@users.noreply.github.com>
2022-04-20 17:13:35 -0300

    Meu primeiro commit

---
+++ src/Components/Botao/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss'
+
+class Botao extends React.Component{
+    render(){
+
+        return(
+            <button className='botao'>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao;
---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../Components/Formulario';
+import Lista from '../Components/Lista';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+     <Formulario/>
+     <Lista/>
+    </div>
+  );
+}
+
+export default App;
